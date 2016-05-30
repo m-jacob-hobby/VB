66bd11e28702ce3a07e18b09b4a611f97b28c180
m-jacob-hobby <m.jacob.hobby@gmail.com>
2016-05-30 20:54:29 +0000

    Add files via upload

---
+++ Homework3/Homework3/Homework3.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Homework3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RateLabel = New System.Windows.Forms.Label()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ResultsLabel = New System.Windows.Forms.Label()
+        Me.ResultsTextBox = New System.Windows.Forms.TextBox()
+        Me.RatingComboBox = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'RateLabel
+        '
+        Me.RateLabel.AutoSize = True
+        Me.RateLabel.Location = New System.Drawing.Point(15, 13)
+        Me.RateLabel.Name = "RateLabel"
+        Me.RateLabel.Size = New System.Drawing.Size(101, 13)
+        Me.RateLabel.TabIndex = 0
+        Me.RateLabel.Text = "Rate cafeteria food:"
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.AutoSize = True
+        Me.SubmitButton.Location = New System.Drawing.Point(69, 37)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(83, 23)
+        Me.SubmitButton.TabIndex = 2
+        Me.SubmitButton.Text = "Submit Rating"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'ResultsLabel
+        '
+        Me.ResultsLabel.AutoSize = True
+        Me.ResultsLabel.Location = New System.Drawing.Point(12, 63)
+        Me.ResultsLabel.Name = "ResultsLabel"
+        Me.ResultsLabel.Size = New System.Drawing.Size(76, 13)
+        Me.ResultsLabel.TabIndex = 3
+        Me.ResultsLabel.Text = "Survey results:"
+        '
+        'ResultsTextBox
+        '
+        Me.ResultsTextBox.Location = New System.Drawing.Point(14, 85)
+        Me.ResultsTextBox.Multiline = True
+        Me.ResultsTextBox.Name = "ResultsTextBox"
+        Me.ResultsTextBox.ReadOnly = True
+        Me.ResultsTextBox.Size = New System.Drawing.Size(186, 165)
+        Me.ResultsTextBox.TabIndex = 4
+        '
+        'RatingComboBox
+        '
+        Me.RatingComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.RatingComboBox.FormattingEnabled = True
+        Me.RatingComboBox.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.RatingComboBox.Location = New System.Drawing.Point(123, 13)
+        Me.RatingComboBox.Name = "RatingComboBox"
+        Me.RatingComboBox.Size = New System.Drawing.Size(75, 21)
+        Me.RatingComboBox.TabIndex = 5
+        '
+        'Homework3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(210, 262)
+        Me.Controls.Add(Me.RatingComboBox)
+        Me.Controls.Add(Me.ResultsTextBox)
+        Me.Controls.Add(Me.ResultsLabel)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.RateLabel)
+        Me.Name = "Homework3"
+        Me.Text = "Cafeteria Survey"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RateLabel As System.Windows.Forms.Label
+    Friend WithEvents SubmitButton As System.Windows.Forms.Button
+    Friend WithEvents ResultsLabel As System.Windows.Forms.Label
+    Friend WithEvents ResultsTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents RatingComboBox As ComboBox
+End Class
---
+++ Homework3/Homework3/Homework3.vb
@@ -0,0 +1,56 @@
+﻿' Project: Homework 3
+' Description: Allows user to enter twenty rating values for the cafeteria, one at a time, and updates an output field displaying the number of ratings per value
+' Author: M Jacob Hobby, m.jacob.hobby@gmail.com
+' Date: 03/16/16
+
+Public Class Homework3
+    Dim RatingArray() As String = {"", "", "", "", "", "", "", "", "", ""}                      ' Array that will generate the bar graphs for # of ratings
+    Dim RatingsCountInteger As Integer = 0                                                      ' Records the number of times ratings have been submitted
+
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        Dim RatingPositionInteger As Integer                                                    ' The position of the RatingArray that user has submitted a rating for
+        Dim CountInteger As Integer = 1                                                         ' Output for rating value into ResultsTextBox
+        Dim RatingOutputString As String = "Rating   Frequency"                                 ' Header text output to ResultsTextBox
+        Dim RatingString As String                                                              ' The string value obtained from the RatingComboBox
+
+        Try
+            ' Get the value the user selected from the combo box
+            RatingString = RatingComboBox.SelectedItem.ToString
+
+            ' Parse string rating to its int value and modify the array bar graph
+            RatingPositionInteger = Integer.Parse(RatingString)
+            RatingArray(RatingPositionInteger - 1) &= "* "
+
+            ResultsTextBox.Text = RatingOutputString & vbCrLf
+
+            ' Output to the ResultsTextBox each rating value followed by its associated number of ratings as bar graph
+            For Each RateString As String In RatingArray
+                If CountInteger < 10 Then
+                    ' if rating value is less then 10...
+                    ResultsTextBox.Text &= CountInteger & "           " & RateString & vbCrLf
+                Else
+                    ' if rating is 10 spacing between value and bar is shortened
+                    ResultsTextBox.Text &= CountInteger & "         " & RateString & vbCrLf
+                End If
+                ' increment the count to output each rating value from 1 to 10
+                CountInteger += 1
+            Next
+
+            ' increase the count of number of times user has submitted a survey value
+            RatingsCountInteger += 1
+
+            ' Once 20 ratings have been submitted, disable the submit button
+            If RatingsCountInteger = 20 Then
+                SubmitButton.Enabled = False
+            End If
+
+        Catch ex As Exception
+            ' If the user does not select a value from the drop down menu, output error message
+            MessageBox.Show("You must select a value from the drop down menu.")
+
+        End Try
+
+    End Sub
+
+End Class
